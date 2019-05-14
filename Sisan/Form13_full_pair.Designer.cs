bd5f9bf4c42d3a90ade73354792ce8b810c7c0a2
omv0102 <omv98@outlook.com>
2019-05-14 10:51:14 +0700

    скопировал форму для последнего метода

---
+++ Sisan/Form13_full_pair.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace system_analysis
+{
+    partial class Form13_full_pair
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label_problem = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.button_cross = new System.Windows.Forms.Button();
+            this.button_minimize = new System.Windows.Forms.Button();
+            this.button_back = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_sort = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(322, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 23);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Альтернативы";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(342, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 23);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "Проблема";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 299);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 40;
+            this.dataGridView1.Size = new System.Drawing.Size(700, 200);
+            this.dataGridView1.TabIndex = 53;
+            this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_CellBeginEdit);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellToolTipTextNeeded += new System.Windows.Forms.DataGridViewCellToolTipTextNeededEventHandler(this.dataGridView1_CellToolTipTextNeeded);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // label_problem
+            // 
+            this.label_problem.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label_problem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_problem.Font = new System.Drawing.Font("Tahoma", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_problem.ForeColor = System.Drawing.Color.Black;
+            this.label_problem.Location = new System.Drawing.Point(50, 85);
+            this.label_problem.Name = "label_problem";
+            this.label_problem.Size = new System.Drawing.Size(700, 27);
+            this.label_problem.TabIndex = 56;
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(118)))), ((int)(((byte)(168)))));
+            this.btn_save.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_save.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOrange;
+            this.btn_save.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_save.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_save.Location = new System.Drawing.Point(230, 531);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(244, 52);
+            this.btn_save.TabIndex = 57;
+            this.btn_save.Text = "Сохранить";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // button_cross
+            // 
+            this.button_cross.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_cross.BackColor = System.Drawing.SystemColors.Control;
+            this.button_cross.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_cross.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_cross.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_cross.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_cross.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_cross.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_cross.ForeColor = System.Drawing.Color.Black;
+            this.button_cross.Location = new System.Drawing.Point(761, -1);
+            this.button_cross.Name = "button_cross";
+            this.button_cross.Size = new System.Drawing.Size(40, 40);
+            this.button_cross.TabIndex = 67;
+            this.button_cross.TabStop = false;
+            this.button_cross.Text = "✖";
+            this.button_cross.UseVisualStyleBackColor = false;
+            this.button_cross.Click += new System.EventHandler(this.button_cross_Click);
+            // 
+            // button_minimize
+            // 
+            this.button_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_minimize.BackColor = System.Drawing.SystemColors.Control;
+            this.button_minimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_minimize.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_minimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_minimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minimize.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_minimize.ForeColor = System.Drawing.Color.Black;
+            this.button_minimize.Location = new System.Drawing.Point(715, -14);
+            this.button_minimize.Name = "button_minimize";
+            this.button_minimize.Size = new System.Drawing.Size(40, 40);
+            this.button_minimize.TabIndex = 66;
+            this.button_minimize.TabStop = false;
+            this.button_minimize.Text = "-";
+            this.button_minimize.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_minimize.UseVisualStyleBackColor = false;
+            this.button_minimize.Click += new System.EventHandler(this.button_minimize_Click);
+            // 
+            // button_back
+            // 
+            this.button_back.BackColor = System.Drawing.SystemColors.Control;
+            this.button_back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_back.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_back.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_back.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_back.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(238)))), ((int)(((byte)(240)))));
+            this.button_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_back.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_back.ForeColor = System.Drawing.Color.Black;
+            this.button_back.Location = new System.Drawing.Point(-1, -14);
+            this.button_back.Name = "button_back";
+            this.button_back.Size = new System.Drawing.Size(61, 40);
+            this.button_back.TabIndex = 68;
+            this.button_back.TabStop = false;
+            this.button_back.Text = "←";
+            this.button_back.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_back.UseVisualStyleBackColor = false;
+            this.button_back.Click += new System.EventHandler(this.button_back_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(46, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(515, 19);
+            this.label4.TabIndex = 70;
+            this.label4.Text = "Для оценивания щелкните по ячейке с оценкой и введите значение.";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(0, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(800, 5);
+            this.label5.TabIndex = 71;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(278, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(248, 29);
+            this.label6.TabIndex = 72;
+            this.label6.Text = "Метод предпочтения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(46, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 38);
+            this.label2.TabIndex = 73;
+            this.label2.Text = "Этот label заполняется в коде\r\nпри загрузке формы.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_sort
+            // 
+            this.btn_sort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(118)))), ((int)(((byte)(168)))));
+            this.btn_sort.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_sort.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOrange;
+            this.btn_sort.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.btn_sort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_sort.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_sort.Location = new System.Drawing.Point(584, 521);
+            this.btn_sort.Name = "btn_sort";
+            this.btn_sort.Size = new System.Drawing.Size(157, 62);
+            this.btn_sort.TabIndex = 74;
+            this.btn_sort.Text = "Сортировать по оценкам";
+            this.btn_sort.UseVisualStyleBackColor = false;
+            this.btn_sort.Click += new System.EventHandler(this.btn_sort_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(46, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(691, 38);
+            this.label3.TabIndex = 75;
+            this.label3.Text = "После проставления оценок для наглядности можно расположить альтернативы в порядк" +
+    "е\r\nубывания их значимости по введенным оценкам. Для этого нажмите кнопку \"Сортир" +
+    "овать\".";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form12_prefer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button_back;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_sort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_back);
+            this.Controls.Add(this.button_cross);
+            this.Controls.Add(this.button_minimize);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.label_problem);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form12_prefer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Метод предпочтения";
+            this.Load += new System.EventHandler(this.Form11_rang_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form7_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label_problem;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button button_cross;
+        private System.Windows.Forms.Button button_minimize;
+        private System.Windows.Forms.Button button_back;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_sort;
+        private System.Windows.Forms.Label label3;
+    }
+}
