90f5c3ee9fba57346825f5f479927b60515abcef
Misha <omv98@outlook.com>
2019-05-21 14:57:30 +0700

    метод 0 нарисовал

--- Sisan/Form5.Designer.cs
+++ Sisan/Form5.Designer.cs
@@ -42,6 +42,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.listBox0_weight = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -158,12 +162,16 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoScroll = true;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this.listBox0_weight);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.listBox0_alt);
-            this.panel1.Location = new System.Drawing.Point(12, 338);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(12, 322);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1076, 363);
+            this.panel1.Size = new System.Drawing.Size(1076, 336);
             this.panel1.TabIndex = 63;
             // 
             // label3
@@ -171,7 +179,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(10, 7);
+            this.label3.Location = new System.Drawing.Point(49, 8);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(165, 38);
             this.label3.TabIndex = 67;
@@ -185,7 +193,7 @@
             this.listBox0_alt.FormattingEnabled = true;
             this.listBox0_alt.HorizontalScrollbar = true;
             this.listBox0_alt.ItemHeight = 19;
-            this.listBox0_alt.Location = new System.Drawing.Point(3, 142);
+            this.listBox0_alt.Location = new System.Drawing.Point(3, 118);
             this.listBox0_alt.Name = "listBox0_alt";
             this.listBox0_alt.Size = new System.Drawing.Size(211, 194);
             this.listBox0_alt.TabIndex = 67;
@@ -218,7 +226,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(431, 281);
+            this.label2.Location = new System.Drawing.Point(431, 289);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(248, 23);
             this.label2.TabIndex = 66;
@@ -231,10 +239,57 @@
             this.listBox0_weight.FormattingEnabled = true;
             this.listBox0_weight.HorizontalScrollbar = true;
             this.listBox0_weight.ItemHeight = 19;
-            this.listBox0_weight.Location = new System.Drawing.Point(213, 142);
+            this.listBox0_weight.Location = new System.Drawing.Point(213, 118);
             this.listBox0_weight.Name = "listBox0_weight";
             this.listBox0_weight.Size = new System.Drawing.Size(71, 194);
             this.listBox0_weight.TabIndex = 68;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(86, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 27);
+            this.comboBox1.TabIndex = 70;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(3, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 19);
+            this.label4.TabIndex = 69;
+            this.label4.Text = "Эксперт:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label8.Location = new System.Drawing.Point(211, 96);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 19);
+            this.label8.TabIndex = 71;
+            this.label8.Text = "Вес";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(3, 96);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 19);
+            this.label9.TabIndex = 72;
+            this.label9.Text = "Альтернативы";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // form5_analyst_report
             // 
@@ -242,7 +297,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.CancelButton = this.button_back;
-            this.ClientSize = new System.Drawing.Size(1100, 713);
+            this.ClientSize = new System.Drawing.Size(1100, 670);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.list_solution);
@@ -283,5 +338,9 @@
         private System.Windows.Forms.Label label3;
         public System.Windows.Forms.ListBox listBox0_alt;
         public System.Windows.Forms.ListBox listBox0_weight;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
